2fe03d236188a3795abee0b2c0d1ee1f7101996d
Ha29la10 <115113036+Ha29la10@users.noreply.github.com>
2023-11-10 23:15:17 +0200

    Task_1:LoginAndRegstriationSysytem

--- main.cpp
+++ main.cpp
@@ -1,133 +1,93 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
+
 using namespace std;
 
-struct Customer {
-    string name;
-    int roomNumber;
-    int duration;
-    double bill;
-
-    Customer(const string& n, int room, int dur, double b) : name(n), roomNumber(room), duration(dur), bill(b) {}
+struct User {
+    string username;
+    string password;
 };
 
-vector<Customer> customers;
+void registerUser() {
+    User newUser;
+    cout << "Enter a username: ";
+    cin >> newUser.username;
 
-void saveDataToFile() {
-    ofstream outFile("customerData.txt");
-    if (outFile.is_open()) {
-        for (const auto& customer : customers) {
-            outFile << customer.name << " " << customer.roomNumber << " " << customer.duration << " " << customer.bill << "\n";
-        }
-        outFile.close();
-    }
-}
+    ifstream usersFile("users.txt");
+    string username;
+    bool exists = false;
 
-void displayAllottedRooms() {
-    cout << "Total Allotted Rooms: " << customers.size() <<endl;
-}
-
-void bookRoom() {
-    string name;
-    int roomNumber, duration;
-    double bill;
-
-    cout << "Enter customer name: ";
-    cin >> name;
-    cout << "Enter room number: ";
-    cin >> roomNumber;
-    cout << "Enter duration of stay (in days): ";
-    cin >> duration;
-    cout << "Enter total bill: ";
-    cin >> bill;
-
-    Customer newCustomer(name, roomNumber, duration, bill);
-    customers.push_back(newCustomer);
-    saveDataToFile();
-    cout << "Room booked successfully!" <<endl;
-}
-
-void displayCustomerDetails() {
-    int roomNumber;
-    cout << "Enter room number to search for customer details: ";
-    cin >> roomNumber;
-
-    bool found = false;
-    for (const auto& customer : customers) {
-        if (customer.roomNumber == roomNumber) {
-            cout << "Customer Name: " << customer.name << endl;
-            cout << "Room Number: " << customer.roomNumber << endl;
-            cout << "Duration of Stay: " << customer.duration << " days" <<endl;
-            cout << "Total Bill: $" << customer.bill << endl;
-            found = true;
+    while (usersFile >> username) { //reads and store in username
+        if (username == newUser.username) {
+            exists = true;
             break;
         }
     }
-
-    if (!found) {
-        cout << "Customer with room number " << roomNumber << " not found." << endl;
+    usersFile.close();
+    // check if user exists
+    if (exists) {
+        cout << "User already exists. Please try a different username." << endl;
+        return;
     }
+
+    cout << "Enter a password: ";
+    cin >> newUser.password;
+
+    ofstream outFile("users.txt", ios::app);
+    outFile << newUser.username << " " << newUser.password << endl;
+    outFile.close();
+    cout << "Registration successful!" << endl;
 }
 
-void deleteCustomerRecord() {
-    int roomNumber;
-    cout << "Enter room number to delete customer record: ";
-    cin >> roomNumber;
+void loginUser() {
+    User user;
+    cout << "Enter your username: ";
+    cin >> user.username;
+    cout << "Enter your password: ";
+    cin >> user.password;
 
-    for (auto it = customers.begin(); it != customers.end(); ++it) {
-        if (it->roomNumber == roomNumber) {
-            customers.erase(it);
-            saveDataToFile();
-            cout << "Customer record deleted successfully." << endl;
-            return;
+    ifstream usersFile("users.txt");
+    string username, password;
+    bool loggedIn = false;
+
+    while (usersFile >> username >> password) {
+        if (username == user.username && password == user.password) {
+            loggedIn = true;
+            break;
         }
     }
-    cout << "Customer with room number " << roomNumber << " not found." << endl;
+    usersFile.close();
+
+    if (loggedIn) {
+        cout << "Login successful! Welcome, " << user.username << "!" << endl;
+    } else {
+        cout << "Invalid username or password. Please try again." << endl;
+    }
 }
 
 int main() {
-    ifstream inFile("customerData.txt");
-    if (inFile.is_open()) {
-        string name;
-        int roomNumber, duration;
-        double bill;
-
-        while (inFile >> name >> roomNumber >> duration >> bill) {
-            Customer loadedCustomer(name, roomNumber, duration, bill);
-            customers.push_back(loadedCustomer);
-        }
-        inFile.close();
-    }
-
     int choice;
     do {
-        cout << "\nHotel Management System\n";
-        cout << "1. Book Room\n2. Search Customer Details\n3. Delete Customer Record\n4. Total Allotted Rooms\n5. Exit\nEnter your choice: ";
+        cout << "Menu:\n1. Register\n2. Login\n3. Exit\nEnter your choice: ";
         cin >> choice;
 
         switch (choice) {
             case 1:
-                bookRoom();
+                registerUser();
                 break;
             case 2:
-                displayCustomerDetails();
+                loginUser();
                 break;
             case 3:
-                deleteCustomerRecord();
-                break;
-            case 4:
-                displayAllottedRooms();
-                break;
-            case 5:
-                cout << "Exiting the system. Goodbye!" << endl;
+                cout << "Exiting the program. Goodbye!" << endl;
                 break;
             default:
                 cout << "Invalid choice. Please enter a valid option." << endl;
                 break;
         }
-    } while (choice != 5);
+    } while (choice != 3);
 
     return 0;
 }
+
